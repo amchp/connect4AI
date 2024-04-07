2eb84ddde043b797d159b32e025000a7d3583547
Alejandro McEwen <juanmcewen@Juans-Mac-mini.local>
2024-04-07 12:42:43 -0500

    Done with training code bug fixing insues

--- connect4.py
+++ connect4.py
@@ -1,3 +1,6 @@
+from typing import Union
+
+
 GRID_ROWS = 6
 GRID_COLUMNS = 7
 CONNECT_TILES = 4
@@ -48,19 +51,33 @@ class Connect4:
             ):
                 return True
         return False
+    
+    def make_state(self) -> list[int]:
+        state = [
+            self.grid[i][j]
+            for i in range(len(self.grid))
+            for j in range(len(self.grid[i]))
+        ]
+        state.append(self.turn)
+        return state
 
-    def move(self, position: int) -> int:
+    def move(self, position: int) -> Union[list[int], int, int]:
         if self.heights[position] < 0:
-            return -2
+            return (self.make_state(), -1000, 0)
         curent_height = self.heights[position]
         self.grid[curent_height][position] = self.turn
         self.heights[position] -= 1
+        self.num_move += 1
         if self.connect_4(curent_height, position):
-            return self.turn
+            return (self.make_state(), self.num_move, self.turn)
+        if sum(self.heights) == -7:
+            return (self.make_state(), self.num_move, 2)
         self.turn *= -1
-        return 0
+        return (self.make_state(), self.num_move, 0)
 
-    def reset(self):
+    def reset(self) -> list[int]:
         self.grid = [[0 for _ in range(GRID_COLUMNS)] for _ in range(GRID_ROWS)]
         self.turn = 1
         self.heights = [GRID_ROWS - 1 for _ in range(GRID_COLUMNS)]
+        self.num_move = 1
+        return self.make_state()
